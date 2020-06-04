81ff53cf6de60fb2e45550bc2d965639fb3aefe5
Dylan Russell <dyl.russell@gmail.com>
2020-06-03 14:32:13 -1000

    fixed - working on Mac

--- R/dbcon.R
+++ R/dbcon.R
@@ -1,5 +1,7 @@
-con <- DBI::dbConnect(RPostgres::Postgres(),dbname = 'postgres',
+conn <- DBI::dbConnect(RPostgres::Postgres(),dbname = 'postgres',
                  host = '34.94.46.211', # Public IP of the PostgresSQL database
                  port = 5432,
                  user = rstudioapi::askForPassword("Database user"),
-                 password = rstudioapi::askForPassword("Database password"))
+                 password = rstudioapi::askForPassword("Database password")
+                 )
+
