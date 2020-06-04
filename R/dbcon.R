65ed076531722ada93512e9ef64071825f25bfb3
Dylan Russell <dyl.russell@gmail.com>
2020-06-03 22:40:06 -1000

    updating dbcon.r

--- R/dbcon.R
+++ R/dbcon.R
@@ -1,7 +1,27 @@
-conn <- DBI::dbConnect(RPostgres::Postgres(),dbname = 'postgres',
-                 host = '34.94.46.211', # Public IP of the PostgresSQL database
-                 port = 5432,
-                 user = rstudioapi::askForPassword("Database user"),
-                 password = rstudioapi::askForPassword("Database password")
-                 )
+con <- DBI::dbConnect(RPostgres::Postgres(),
+                      dbname = nsqip_db,
+                      host = nsqip_db_ip, # Public IP of the PostgresSQL database
+                      port = nsqip_db_port,
+                      user = nsqip_db_user,
+                      password = nsqip_db_pw)
 
+con <- DBI::dbConnect(odbc::odbc(),
+                      Driver   = nsqip_db_driver,
+                      Server   = nsqip_db_ip, # Public IP of the PostgresSQL database
+                      Database = nsqip_db,
+                      UID      = nsqip_db_user,
+                      PWD      = nsqip_db_pw,
+                      Port     = nsqip_db_port) # odbc() allows interface with RStudio connections pane.
+
+addToNsqip <- function(file) {
+  df <- readr::read_delim(file,
+                    delim = "\t",
+                    na = c("","NA","-99","NULL"),
+                    )
+  DBI::dbCreateTable(con,
+                     name = 'puf',
+                     fields = df)
+  DBI::dbAppendTable(con,
+                     name = 'puf',
+                     value = df)
+}
