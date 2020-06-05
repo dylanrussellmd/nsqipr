11181e77d735160352497e4cfa2f60c5d541629f
Dylan Russell <dyl.russell@gmail.com>
2020-06-05 12:04:12 -1000

    adds database append functions

--- R/dbcon.R
+++ R/dbcon.R
@@ -1,52 +1,56 @@
-#conn <- DBI::dbconnnect(odbc::odbc(),
+#conn <- DBI::dbConnect(odbc::odbc(),
 #                      Driver   = nsqip_db_driver,
-#                      Server   = nsqip_db_ip, # Public IP of the PostgreSQL database
+#                      Server   = nsqip_db_ip,
 #                      Database = nsqip_db,
 #                      UID      = nsqip_db_user,
 #                      PWD      = nsqip_db_pw,
-#                      Port     = nsqip_db_port) # odbc() allows interface with RStudio connnections pane.
+#                      Port     = nsqip_db_port
+#)
 
-connDb <- function(driver, ip, db, port, user, pw) {
-  DBI::dbconnnect(odbc::odbc(),
-                 Driver = driver,
-                 Server = ip,
-                 Databse = db,
-                 Port = port,
-                 UID = user,
-                 PWD = pw
-                 )
+#conn <- function(driver, ip, db, port, user, pw) {
+#  DBI::dbConnect(odbc::odbc(),
+#                 Driver = driver,
+#                 Server = ip,
+#                 Databse = db,
+#                 Port = port,
+#                 UID = user,
+#                 PWD = pw
+#                 )
+#}
+
+import_data_dir <- function(conn, dir) {
+  if (file_test("-d", dir)) {
+    files <- list.files(path = dir, pattern = "*.txt",
+                        full.names = TRUE, recursive = FALSE)
+    lapply(files, write_data, conn)
+  } else if (file_test("-f", dir)) {
+    write_data(conn, dir)
+  } else {
+    stop(paste(dir,'is an invalid file or directory path.', sep = " "))
+  }
 }
 
-addToNsqip <- function(file) {
-  df <- readr::read_delim(file,
-                    delim = "\t",
-                    na = c("","NA","-99","NULL")
-                    )
-  DBI::dbCreateTable(conn,
-                     name = 'puf',
-                     fields = df)
-  DBI::dbAppendTable(conn,
-                     name = 'puf',
-                     value = df)
+write_data <- function(conn, file) {
+  df <- create_df(file)
+  tablename <- parse_filename(file)
+  if (!DBI::dbExistsTable(conn, tablename)) {
+    DBI::dbCreateTable(conn, tablename, df)
+  }
+  DBI::dbAppendTable(conn, tablename, df)
 }
 
-importData <- function(driver, ip, db, port, user, pw, dir) {
-  conn <- connDb(driver, ip, db, port, user, pw)
-  files <- list.files(path = dir, pattern = "*.txt", full.names = TRUE, recursive = FALSE)
-  lapply(files, writeData, conn)
-}
-
-writeData <- function(file, conn) {
-  createDF(file)
-}
-
-parseFilename <- function(file) {
-  pattern <- stringr::regex("acs_nsqip_puf|puf_tar_[a-z]{1,4}", ignore_case = TRUE)
+parse_filename <- function(file) {
+  pattern <- stringr::regex("acs_nsqip_puf|puf_tar_[a-z]{1,4}",
+                            ignore_case = TRUE)
   stopifnot(stringr::str_detect(file, pattern))
   tablename <- stringr::str_extract(file, pattern)
   stringr::str_to_lower(tablename)
 }
 
-assertTableExists <- function(conn, tablename) {
-  stopifnot(DBI::dbExistsTable(conn, tablename))
+create_df <- function(file) {
+  nas <- c("", "NA", "-99","NULL")
+  df <- readr::read_tsv(file, na = nas, guess_max = 30000,
+                          trim_ws = TRUE) %>%
+    dplyr::rename_all(tolower) %>%
+    dplyr::mutate(dplyr::across(where(is.character), stringr::str_to_sentence))
 }
