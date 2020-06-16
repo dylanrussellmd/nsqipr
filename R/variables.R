5fc51c20ebdd3cf28095ad9d786eed5ee97e75b0
Dylan Russell <dyl.russell@gmail.com>
2020-06-15 18:07:30 -1000

    working on clean

--- R/variables.R
+++ R/variables.R
@@ -1,11 +1,11 @@
 new_variable <- function(var = character()) {
-  stopifnot(is_character(var))
+  stopifnot(rlang::is_character(var))
   structure(var, class = "nsqiprVariable")
 }
 
 validate_variable <- function(x) {
   value <- unclass(x)
-  if(is_null(match.arg(value, var_list))) {
+  if(rlang::is_null(match.arg(value, c("test1","test2")))) {
     stop(
       "Variable name is not valid.",
       call. = FALSE
