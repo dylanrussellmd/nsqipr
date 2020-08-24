e05b6468ea1696aff8d16554ce69ad4dce66e27f
Dylan Russell <dyl.russell@gmail.com>
2020-08-23 20:54:12 -1000

    updated documentation

--- R/nsqip.R
+++ R/nsqip.R
@@ -1,10 +1,13 @@
+#' Clean NSQIP data
+#'
 #' Converts a single NSQIP \code{.txt} or a directory of NSQIP \code{.txt} files into a standardized NSQIP data frame.
 #'
 #' @return a data frame of class \code{tibble}.
 #'
 #' @param path path to file or directory.
+#' @param return_df return a data frame.
 #' @param write_to_csv write the resulting data frame to a \code{.csv} file. Produces individual files for each \code{.txt} input
-#' @param append writes the resulting data frames all to a single \code{.csv} file called \code{nsqip_clean.csv}
+#' @param append writes the resulting data frames all to a single \code{.csv} file
 #'
 #' @export
 #' @importFrom "utils" "file_test"
